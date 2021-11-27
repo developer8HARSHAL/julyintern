538bb39ed45e1471f365d90e3299d7a9c9bdbe0a
Harshal pinge <86871141+developer8HARSHAL@users.noreply.github.com>
2021-11-27 13:57:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 const app = express();
-const bodyParser=require('body-parser');
-const dotenv=require('dotenv')
+
+const dotenv = require('dotenv')
 dotenv.config()
-const port =process.env.PORT|| 5785;
+const port = process.env.PORT || 5785;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
-const cors=require('cors')
+const cors = require('cors')
 /*https://eduintern-aug.herokuapp.com*/
 /*const mongourl = "mongodb://localhost:27017"8*/
-const mongourl="mongodb+srv://first:1234@cluster0.ywotg.mongodb.net/eduaug?retryWrites=true&w=majority"
+const mongourl = "mongodb+srv://first:1234@cluster0.ywotg.mongodb.net/eduaug?retryWrites=true&w=majority"
 var db;
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 let col_name = "first"
@@ -22,32 +22,32 @@ app.get('/', (req, res) => {
     res.send('welcome to node api 2')
 })
 
-app.get('/location/',(req,res) =>{
-           db.collection('location').find().toArray((err,result)=>{
-        if(err) throw err;
+app.get('/location/', (req, res) => {
+    db.collection('location').find().toArray((err, result) => {
+        if (err) throw err;
         res.send(result)
     })
 })
 
 app.get('/cuisine', (req, res) => {
-        db.collection('cuisine').find().toArray((err, result) => {
+    db.collection('cuisine').find().toArray((err, result) => {
         if (err) throw err;
         res.send(result)
     })
 })
 app.get('/mealtype', (req, res) => {
-        db.collection('mealType').find().toArray((err, result) => {
+    db.collection('mealType').find().toArray((err, result) => {
         if (err) throw err;
         res.send(result)
     })
 })
-app.get('/restaurant', (req,res) =>{
-        db.collection('restaurant').find().toArray((err,result)=>{
+app.get('/restaurant', (req, res) => {
+    db.collection('restaurant').find().toArray((err, result) => {
         if (err) throw err;
         res.send(result)
     })
 })
-    /////queryparams example/////
+/////queryparams example/////
 /*app.get('/restaurant', (req,res) =>{
     var cityId = req.query.cityId?req.query.cityId:2;
         db.collection('restaurant').find({city: cityId}).toArray((err,result)=>{
@@ -55,7 +55,7 @@ app.get('/restaurant', (req,res) =>{
         res.send(result)
     })
 })*/
-     ////params example////
+////params example////
 /*app.get('/restaurant/:cityId', (req,res) =>{
     var cityId = req.params.cityId;
         db.collection('restaurant').find({city:cityId}).toArray((err,result)=>{
@@ -64,41 +64,41 @@ app.get('/restaurant', (req,res) =>{
     })
 })*/
 
-   //list all Quicksearches
-app.get('/quicksearch',(req,res) =>{
-    db.collection('mealType').find().toArray((err,result)=>{
-        if(err) throw err;
+//list all Quicksearches
+app.get('/quicksearch', (req, res) => {
+    db.collection('mealType').find().toArray((err, result) => {
+        if (err) throw err;
         res.send(result)
     })
 })
 
 // restaurant Details
-app.get('/details/:id',(req,res) => {
+app.get('/details/:id', (req, res) => {
     var id = req.params.id
-    db.collection('restaurents').find({_id:id}).toArray((err,result)=>{
-        if(err) throw err;
+    db.collection('restaurents').find({ _id: id }).toArray((err, result) => {
+        if (err) throw err;
         res.send(result)
     })
 })
 
 // place order 
-app.post('/placeOrder',(req,res) => {
+app.post('/placeOrder', (req, res) => {
     console.log(req.body);
-    db.collection('orders').insert(req.body,(err,result) => {
-        if(err) throw err;
+    db.collection('orders').insert(req.body, (err, result) => {
+        if (err) throw err;
         res.send("Order Placed")
     })
 })
 
 //filterapi
-app.get('/filter/:mealType',(req,res)=>{
+app.get('/filter/:mealType', (req, res) => {
     var mealType = req.params.mealType;
-    var query = {"Type:mealtype":mealType};
-    if(req.query.cuisine){
-        query={"Type:mealtype.mealtype":mealType,"cuisine.cuisine":req.query.cuisine}
+    var query = { "Type:mealtype": mealType };
+    if (req.query.cuisine) {
+        query = { "Type:mealtype.mealtype": mealType, "cuisine.cuisine": req.query.cuisine }
     }
     db.collection('restaurant').find(query).toArray.toArray((err, result) => {
-        if(err)throw err;
+        if (err) throw err;
         res.send(result)
     })
 })
